61cc3d68d4c6a291f644f1a1ebe3c4fd3d1e9f64
Haoyan Huo <hhaoyann@gmail.com>
2020-05-10 12:52:27 -0700

    Install wheels at run time.

--- Dockerfile
+++ Dockerfile
@@ -8,23 +8,6 @@ RUN apk add libstdc++ libgcc libxslt libxml2 openssl libffi
 #   make automake gcc g++ python3-dev linux-headers \
 #   libffi-dev openssl-dev libxml2-dev libxslt-dev
 RUN pip install --upgrade pip
-RUN pip install  /scraper/wheels/bcrypt-3.1.7-cp36-cp36m-linux_x86_64.whl\
-    /scraper/wheels/cffi-1.14.0-cp36-cp36m-linux_x86_64.whl\
-    /scraper/wheels/cryptography-2.9.1-cp36-cp36m-linux_x86_64.whl\
-    /scraper/wheels/lxml-4.5.0-cp36-cp36m-linux_x86_64.whl\
-    /scraper/wheels/numpy-1.18.2-cp36-cp36m-linux_x86_64.whl\
-    /scraper/wheels/pandas-1.0.3-cp36-cp36m-linux_x86_64.whl\
-    /scraper/wheels/pdfminer-20191125-py3-none-any.whl\
-    /scraper/wheels/Protego-0.1.16-py3-none-any.whl\
-    /scraper/wheels/pycryptodome-3.9.7-cp36-cp36m-linux_x86_64.whl\
-    /scraper/wheels/PyDispatcher-2.0.5-py3-none-any.whl\
-    /scraper/wheels/pymongo-3.10.1-cp36-cp36m-linux_x86_64.whl\
-    /scraper/wheels/PyNaCl-1.3.0-cp36-cp36m-linux_x86_64.whl\
-    /scraper/wheels/PyPDF2-1.26.0-py3-none-any.whl\
-    /scraper/wheels/pysftp-0.2.9-py3-none-any.whl\
-    /scraper/wheels/Twisted-20.3.0-cp36-cp36m-linux_x86_64.whl\
-    /scraper/wheels/zope.interface-5.1.0-cp36-cp36m-linux_x86_64.whl
-RUN pip install -r /scraper/requirements.txt
 
 # Cleanup
 #RUN apk del .build-deps
--- job.py
+++ job.py
@@ -1,8 +1,35 @@
 import os
+import tempfile
 import time
+import urllib.request
 from threading import Thread
 
-jobs = {
+from pip._internal.commands.install import InstallCommand
+
+current_dir = os.path.realpath(os.path.dirname(__file__))
+os.chdir(current_dir)
+
+wheels_base = 'https://github.com/COVID-19-Text-Mining/scrapers/raw/master/wheels/'
+wheels = [
+    "bcrypt-3.1.7-cp36-cp36m-linux_x86_64.whl",
+    "cffi-1.14.0-cp36-cp36m-linux_x86_64.whl",
+    "cryptography-2.9.1-cp36-cp36m-linux_x86_64.whl",
+    "lxml-4.5.0-cp36-cp36m-linux_x86_64.whl",
+    "numpy-1.18.2-cp36-cp36m-linux_x86_64.whl",
+    "pandas-1.0.3-cp36-cp36m-linux_x86_64.whl",
+    "pdfminer-20191125-py3-none-any.whl",
+    "Protego-0.1.16-py3-none-any.whl",
+    "pycryptodome-3.9.7-cp36-cp36m-linux_x86_64.whl",
+    "PyDispatcher-2.0.5-py3-none-any.whl",
+    "pymongo-3.10.1-cp36-cp36m-linux_x86_64.whl",
+    "PyNaCl-1.3.0-cp36-cp36m-linux_x86_64.whl",
+    "PyPDF2-1.26.0-py3-none-any.whl",
+    "pysftp-0.2.9-py3-none-any.whl",
+    "Twisted-20.3.0-cp36-cp36m-linux_x86_64.whl",
+    "zope.interface-5.1.0-cp36-cp36m-linux_x86_64.whl",
+]
+
+jobs_registry = {
     'scrapy crawl biorxiv': 3600,
     'scrapy crawl cord_19': 3600,
     'scrapy crawl chemrxiv': 3600,
@@ -13,49 +40,74 @@ jobs = {
 }
 
 
-def run_job(job):
-    os.system(job)
+def install_pip():
+    temp_dir = tempfile.gettempdir()
+    wheel_fn = []
+    for wheel in wheels:
+        fn = os.path.join(temp_dir, wheel)
+
+        print(f'{wheels_base + wheel} => {fn}')
+        urllib.request.urlretrieve(wheels_base + wheel, fn)
+
+        wheel_fn.append(fn)
+
+    install_cmd = InstallCommand('install', 'install packages')
+    if install_cmd.main(wheel_fn) or install_cmd.main(['-r', 'requirements.txt']):
+        print('Failed to execute pip install.')
+        exit(1)
+
+    for wheel in wheel_fn:
+        print(f'Removing {wheel}')
+        os.unlink(wheel)
+
+
+class Scheduler(object):
+    def __init__(self, jobs):
+        self.jobs = jobs.copy()
+        self.timer = jobs.copy()
+        self.running = {name: None for name in jobs}
+
+        for job in jobs:
+            self.reset_job(job)
+
+    def poll_jobs(self):
+        for name in self.running:
+            if self.running[name] is not None and not self.running[name].is_alive():
+                self.running[name].join()
+                print(f'Job {name} finished.')
+                self.running[name] = None
+
+    def reset_job(self, name):
+        print(f'Scheduling job "{name}" to be run in {self.jobs[name]} seconds...')
+        self.timer[name] = self.jobs[name]
+
+    @staticmethod
+    def run_job(job):
+        os.system(job)
+
+    def run_forever(self):
+        while True:
+            for name in list(self.timer):
+                self.timer[name] -= 1
+
+            self.poll_jobs()
+
+            execution = [x for x in self.timer if self.timer[x] <= 0]
+            for job in execution:
+                if self.running[job] is not None:
+                    print(f'Job {job} already running, skipping execution')
+                else:
+                    print(f'Running job "{job}"')
+                    t = Thread(target=self.run_job, args=(job,))
+                    t.start()
+                    self.running[job] = t
+
+                self.reset_job(job)
+
+            time.sleep(1)
 
 
 if __name__ == '__main__':
-    current_dir = os.path.realpath(os.path.dirname(__file__))
-    os.chdir(current_dir)
+    install_pip()
 
-    timer = jobs.copy()
-    running = {name: None for name in jobs}
-
-    def poll_jobs():
-        for name in running:
-            if running[name] is not None and not running[name].is_alive():
-                running[name].join()
-                print(f'Job {name} finished.')
-                running[name] = None
-
-
-    def reset_job(name):
-        print(f'Scheduling job "{name}" to be run in {jobs[name]} seconds...')
-        timer[name] = jobs[name]
-
-
-    for job in jobs:
-        reset_job(job)
-
-    while True:
-        for name in list(timer):
-            timer[name] -= 1
-
-        poll_jobs()
-
-        execution = [x for x in timer if timer[x] == 0]
-        for job in execution:
-            if running[job] is not None:
-                print(f'Job {job} already running, skipping execution')
-            else:
-                print(f'Running job "{job}"')
-                t = Thread(target=run_job, args=(job,))
-                t.start()
-                running[job] = t
-
-            reset_job(job)
-
-        time.sleep(1)
+    Scheduler(jobs_registry).run_forever()
