219202e500f1dc66d8bdf061469f92e1a3fd1922
aksub99 <akshaysubramanian99@gmail.com>
2020-05-24 11:35:14 +0530

    Toggle ROBOTSTXT_OBEY

--- covidscholar_scraper/settings.py
+++ covidscholar_scraper/settings.py
@@ -21,7 +21,7 @@ USER_AGENT = 'COVID-19 Scholar: Text-mining for COVID-19 research @ LBNL ' \
              '(+covid19textmining@googlegroups.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
