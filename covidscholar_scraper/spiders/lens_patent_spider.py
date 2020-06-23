b15cd23b2f65d0f7892d07df020fb602f6d4a01b
Haoyan Huo <hhaoyann@gmail.com>
2020-06-23 10:11:39 -0700

    Fix lens

--- covidscholar_scraper/spiders/lens_patent_spider.py
+++ covidscholar_scraper/spiders/lens_patent_spider.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import datetime
-from urllib.parse import urlencode, urljoin
+import json
+import re
+from urllib.parse import urlencode
 
 import scrapy
 from pymongo import HASHED
-from scrapy.utils.markup import remove_tags
 
 from ._base import BaseSpider
 
@@ -20,76 +21,59 @@ class PatentSpider(BaseSpider):
     }
 
     @staticmethod
-    def build_lens_url(**kwargs):
-        base_url = 'https://www.lens.org/lens/search/collection/179940'
+    def build_lens_url(kwargs, from_i=0):
+        base_url = 'https://www.lens.org/lens/api/search/patent'
         query_dict = {
-            'dates': '+pub_date:19740101-20200402',  # dates
-            'l': 'en',  # language
+            'q': '',
             'st': 'true',
-            'n': 50,
-            'p': 0,
-            'v': 'table',
-            's': 'pub_date',  # sort by
-            'd': '+'
+            'collectionId': 179940,
+            'e': 'false',
+            'f': 'false',
+            'l': 'en',
+            'publishedDate.from': '2020-01-01',
+            'publishedDate.to': '2020-06-23'
         }
         query_dict.update(kwargs)
-        return '%s?%s' % (base_url, urlencode(query_dict))
+
+        payload = json.dumps({
+            "size": "50",
+            "from": from_i,
+            "sort": [
+                {"pub_date": {"order": "desc"}}
+            ]})
+        return ('%s?%s' % (base_url, urlencode(query_dict))), payload
 
     def start_requests(self):
-        today = datetime.datetime.now().strftime("%Y%m%d")
+        today = datetime.datetime.now().strftime("%Y-%m-%d")
+        three_mo = (datetime.datetime.now() - datetime.timedelta(days=90)).strftime("%Y-%m-%d")
 
+        url, payload = self.build_lens_url({
+            'publishedDate.from': three_mo,
+            'publishedDate.to': today,
+        })
         yield scrapy.Request(
-            url=self.build_lens_url(
-                dates=f"+pub_date:20200101-{today}"),
+            url=url,
+            method='POST',
+            headers={
+                'content-type': 'application/json'
+            },
+            body=payload,
             callback=self.parse,
+            meta={'from': 0},
             dont_filter=True)
 
     def parse(self, response):
-        patents_this_page = response.xpath('//*[contains(@class, "div-table-results-row")]')
-        published_date = datetime.datetime(year=2000, month=1, day=1)
+        data = json.loads(response.text)
 
-        for patent in patents_this_page:
-            publication_number = ''.join(
-                patent.xpath(
-                    './/div[contains(@class, "doc-type")]//a/text()').extract()).strip()
+        for patent in data['hits']:
+            publication_number = patent['displayKey']
+            lens_id = patent['lensId']
+            title = patent['titleFallbackToDisplayKey']
 
-            lens_id = ''.join(
-                patent.xpath(
-                    './/div[contains(@class, "lens-id")]//a/text()').extract()).strip()
-
-            title = patent.xpath('.//h3//a/text()').extract_first().strip()
-
-            published_data_raw = {}
-            for entry in patent.xpath('.//ul[contains(@class, "header-meta")]/li'):
-                key = entry.xpath('.//b/text()').extract_first().strip()
-                key = key.strip(':')
-                if not key:
-                    continue
-
-                value = ''.join(entry.xpath('./text()').extract()).strip()
-                published_data_raw[key] = value
-
-            try:
-                published_date = datetime.datetime.strptime(
-                    published_data_raw['Published'].replace(',', ''), '%b %d %Y')
-            except (KeyError, ValueError):
-                published_date = None
-
-            try:
-                filed_date = datetime.datetime.strptime(
-                    published_data_raw['Filed'].replace(',', ''), '%b %d %Y')
-            except (KeyError, ValueError):
-                filed_date = None
-
-            try:
-                earliest_priority_date = datetime.datetime.strptime(
-                    published_data_raw['Earliest Priority'].replace(',', ''), '%b %d %Y')
-            except (KeyError, ValueError):
-                earliest_priority_date = None
-
-            applicants = published_data_raw.get('Applicant', [])
-            if applicants:
-                applicants = list(map(str.strip, applicants.split(',')))
+            published_date = datetime.datetime.fromtimestamp(patent['publicationDate'] / 1000)
+            filed_date = datetime.datetime.fromtimestamp(patent['filingDate'] / 1000)
+            earliest_priority_date = datetime.datetime.strptime(patent['earliestPriorityDate'], '%Y-%m-%d')
+            applicants = list(map(str.capitalize, patent['applicants']))
 
             abstract_link = "https://www.lens.org/lens/patent/%s" % (lens_id,)
 
@@ -111,59 +95,48 @@ class PatentSpider(BaseSpider):
                     "Published_Date": published_date,
                     "Filed_Date": filed_date,
                     "Earliest_Priority_Date": earliest_priority_date,
+
+                    "Returned_Data": patent,
                 },
                 priority=10,
             )
 
-        if len(patents_this_page) == 50:
-            next_page_extend_link = response.xpath(
-                './/a[contains(@class, "fa-chevron-right")]/@href')
-            if len(next_page_extend_link) > 0:
-                yield scrapy.Request(
-                    urljoin(
-                        response.request.url,
-                        next_page_extend_link.extract_first().strip()),
-                    callback=self.parse
-                )
-            else:
-                old_date = published_date.strftime("%Y%m%d")
-                today = datetime.datetime.now().strftime("%Y%m%d")
-                new_link = self.build_lens_url(
-                    dates="+pub_date:{old}-{today}".format(
-                        old=old_date, today=today))
-                yield scrapy.Request(
-                    new_link,
-                    callback=self.parse
-                )
+        if len(data['hits']) == 50:
+            today = datetime.datetime.now().strftime("%Y-%m-%d")
+            three_mo = (datetime.datetime.now() - datetime.timedelta(days=90)).strftime("%Y-%m-%d")
+
+            url, payload = self.build_lens_url({
+                'publishedDate.from': three_mo,
+                'publishedDate.to': today,
+            }, from_i=response.meta['from'] + 50)
+            yield scrapy.Request(
+                url=url,
+                method='POST',
+                headers={
+                    'content-type': 'application/json'
+                },
+                body=payload,
+                callback=self.parse,
+                meta={'from': response.meta['from'] + 50},
+                dont_filter=True)
 
     def parse_abstract(self, response):
         meta = response.meta
 
-        abstract_text = remove_tags(response.xpath(
-            './/div[@class="page-title"]/following-sibling::p[1]').extract_first().strip()).strip()
-        meta['Abstract'] = abstract_text
+        abstract_text = json.loads(re.search(
+            r'"abstract"\s*:({.*?\})', response.text).group(1))
+        meta['Abstract'] = abstract_text['text']
 
-        links = response.xpath(".//a/@href").extract()
-        html_link = None
+        meta['HTML_Link'] = "https://www.lens.org/lens/patent/%s/fulltext" % (meta['Lens_ID'],)
 
-        for link in links:
-            if "fulltext" in link:
-                html_link = "https://www.lens.org" + link
-                break
-        meta['HTML_Link'] = html_link
-
-        if html_link is not None:
-            yield scrapy.Request(
-                html_link,
-                callback=self.parse_full_text,
-                meta=meta,
-            )
-        else:
-            meta['Last_Updated'] = datetime.datetime.now()
-            self.save_article(meta, to='Scraper_lens_patents')
+        yield scrapy.Request(
+            meta['HTML_Link'],
+            callback=self.parse_full_text,
+            meta=meta,
+        )
 
     def parse_full_text(self, response):
-        fulltext = response.xpath('.//div[@id="fullText"]').extract_first().strip()
+        fulltext = response.xpath('.//div[@id="fullText"]').extract_first()
         meta = response.meta
         meta['Full_Text'] = fulltext
         meta['Last_Updated'] = datetime.datetime.now()
